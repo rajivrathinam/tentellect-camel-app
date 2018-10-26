104f4fbe7ff725fbea33af551d194acdf0c333ed
rajivrathinam <rajiv.rathinam4@gmail.com>
2018-10-27 00:00:27 +0530

    init

---
+++ src/main/java/com/tentellect/routeapp/RouteapplicationApplication.java
@@ -0,0 +1,12 @@
+package com.tentellect.routeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RouteapplicationApplication.class, args);
+    }
+}
